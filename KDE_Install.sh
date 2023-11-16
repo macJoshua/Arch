ce73e4d1417f3fc8d05c972c258ff185a92ee860
macJoshua <93446867+macJoshua@users.noreply.github.com>
2023-11-15 21:26:29 -0500

    Update KDE_Install.sh

--- KDE_Install.sh
+++ KDE_Install.sh
@@ -9,31 +9,39 @@ echo "framework" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 framework.localdomain framework" >> /etc/hosts
+sudo timedatectl set-ntp true
+
 echo root:password | chpasswd
 
-# You can remove the tlp package if you are installing on a desktop or vm
+#Installs Arch yay package manager
+git clone https://aur.archlinux.org/yay.git
+cd yay/
+makepkg -si --noconfirm
 
-pacman -S efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh acpi acpi_call tlp os-prober ntfs-3g terminus-font
+#Standard System Packages
+yay -S --noconfirm efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh acpi acpi_call acipd tlp os-prober ntfs-3g terminus-font tlp firefox vlc libreoffice-fresh net-tools libreoffice-fresh
 
 # Uncomment the necessary packages
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
+#KDE specific packages
+yay -S --noconfirm wayland sddm plasma konsole dolphin plasma-desktop plasma-meta kde-system-meta
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable sshd
 systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-systemctl enable reflector.timer
+systemctl enable tlp 
 systemctl enable fstrim.timer
 systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
+sudo systemctl enable sddm
 
 useradd -mG [username]
 echo [username]:password | chpasswd
-
+echo "[username] ALL=(ALL) ALL" >> /etc/sudoers.d/username
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
